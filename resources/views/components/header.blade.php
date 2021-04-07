25a221691ebd30da57a82c509b6b30c564bdca53
FedDiSte <33763790+FedDiSte@users.noreply.github.com>
2021-04-07 07:34:20 +0200

    first

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-transparent">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">FedDiSte</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Non</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">So</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Scrivere</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav></div>
---
+++ resources/views/components/skillbar.blade.php
@@ -0,0 +1,66 @@
+<div class="skill-bars">
+    <div class="bar">
+        <div class="info">
+            <span>HTML</span>
+        </div>
+        <div class="progress-line html">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>CSS</span>
+        </div>
+        <div class="progress-line css">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>Java</span>
+        </div>
+        <div class="progress-line java">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>JavaScript</span>
+        </div>
+        <div class="progress-line js">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>Python</span>
+        </div>
+        <div class="progress-line py">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>PHP</span>
+        </div>
+        <div class="progress-line php">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>MySQL</span>
+        </div>
+        <div class="progress-line sql">
+            <span></span>
+        </div>
+    </div>
+    <div class="bar">
+        <div class="info">
+            <span>Dart</span>
+        </div>
+        <div class="progress-line dart">
+            <span></span>
+        </div>
+    </div>
+</div>
